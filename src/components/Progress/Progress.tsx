8132b25a8dac81eeccb65215060608e51d69139c
Maxim <shwave_id404@mail.ru>
2021-11-30 22:25:41 +0300

    sfds

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -22,25 +22,29 @@ const Progress = <ArrayType extends any[],> ({ array, field }: Props<ArrayType>)
     const progressRef   = useRef() as React.MutableRefObject<SVGCircleElement> 
     // Look code below to understand why I decide to use refs for this Component
     
-    const allowedItems = array.filter(item => item[field]).length
+    const allowedItems     = array.filter(item => item[field]).length
     const completedPercent = Math.floor(allowedItems / array.length * 100) || 0 // Need this check cuz 0/0 = NaN
 
     
     useEffect(() => {
-        calcAndUpdateStyles()
+        updateCircumference()
 
-        window.addEventListener("resize", calcAndUpdateStyles)
+        window.addEventListener("resize", updateCircumference)
         
         return () => {
-            window.removeEventListener("resize", calcAndUpdateStyles)
+            window.removeEventListener("resize", updateCircumference)
         }        
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    useEffect(updateStyles, [completedPercent])
+    // Update progressRef line offset on new completedPercent (props)
+    useEffect(
+        updateStyles, 
+        [completedPercent]
+    )
     
-    function calcCircumference() {
+    function updateCircumference() {
         /*
             !WAeRNING:
             Function will calculate circumferece of SVG by getting "r" (radius) value from computed styles.
@@ -48,12 +52,15 @@ const Progress = <ArrayType extends any[],> ({ array, field }: Props<ArrayType>)
             So in CSS media queries you can change just one variable (--canvas-size)
             but script that calculates circumference will stay OK as well as visual part of circle
             
-            P.S. I dont know if it's good solution, would be glad if you said me :)
+            P.S. Solution on ref for this component helps to avoid extra renders on mount
+            P.S.S. I dont know if it's good solution, would be glad if you said me :)
         */
-       const computedStyles = window.getComputedStyle(layoutRef.current) as CSSStyleDeclarationWithRadius
-       const radius         = parseInt(computedStyles.r.replace(/px/, ''), 10)
-    
-       circumference.current = 2 * Math.PI * radius  
+        const computedStyles = window.getComputedStyle(layoutRef.current) as CSSStyleDeclarationWithRadius
+        const radius         = parseInt(computedStyles.r.replace(/px/, ''), 10)
+        
+        circumference.current = 2 * Math.PI * radius 
+       
+        updateStyles()
 
     }
 
@@ -65,13 +72,6 @@ const Progress = <ArrayType extends any[],> ({ array, field }: Props<ArrayType>)
         progressRef.current.style.strokeDasharray  = `${circumference.current} ${circumference.current}`
         progressRef.current.style.strokeDashoffset = `${strokeDashoffset}`
         
-    }
-
-    function calcAndUpdateStyles() {
-
-        calcCircumference()
-        updateStyles()
-
     }
     
     return (
